50a23270a04503ed8d70a5696382ae60424e7c85
almaperry <almaperub@gmail.com>
2026-09-02 10:38:42 +0300

    new

--- new_bar.py
+++ new_bar.py
@@ -1,2 +1,12 @@
-print("hello")
-print("please")
+#1
+print(" Hello Enter your  name: ")
+
+#2
+print(" Hello Enter your adress: ")
+
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
